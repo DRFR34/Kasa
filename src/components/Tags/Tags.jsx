a07479c7ddc8acf7368f2870e91f0198255532ab
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-03-27 11:04:29 +0100

    Adding Tags component

---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Tags.scss'
+export default function Tags({ rentalOfferTags }) {
+
+  return (
+    <div className='tagsContainer'>
+      {rentalOfferTags.map((tag, index) => (
+        <span key={index} className='tagElet'>{tag}</span>))}
+    </div>
+  )
+}
