e1ecbcea7a9779fd2a77ab60070e5297a04f0fa6
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-03-27 10:13:48 +0100

    Adding RentalsGrid component

---
+++ src/components/RentalsGrid/RentalsGrid.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RentalsData from '../../assets/data/rentalsData.json'
+import RentalCard from './RentalCard'
+import "./RentalsGrid.scss"
+
+export default function RentalsGrid() {
+  return (
+    <div className="rentalsGrid">
+      {RentalsData.map((rentalOffer) => (
+                <RentalCard key={rentalOffer.id} rentalOffer={rentalOffer} />
+            ))}
+
+    </div>
+  )
+}
