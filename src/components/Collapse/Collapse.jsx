24810e9b9eda7a6a22ab0f8629320c7e44a6da37
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-03-27 10:05:22 +0100

    Adding Collapse component

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import "../css/Collapse.scss"
+
+export default function Collapse({ title, content }) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleCollapse = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    return (
+        <div className="collapse">
+            <div className="collapseHeader" onClick={toggleCollapse}>
+                <div>{title}</div>
+                <div className="collapseArrow">
+                    {isExpanded ? <i className="fas fa-chevron-up isExpanded"></i> : <i className="fas fa-chevron-up"></i>}
+                </div>
+            </div>
+            <div className={`collapseContent ${isExpanded ? 'isExpanded' : ''}`}>
+                {Array.isArray(content) ? (
+                    content.map((element, index) => <span key={index}>{element}</span>)
+                ) : (
+                    content
+                )}
+            </div>
+        </div>
+    );
+}
+
+
