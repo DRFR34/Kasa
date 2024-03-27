29e3a73dae1b092fbc5f6bd172e740fbdc70091a
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-03-27 03:31:54 +0100

    Adding Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
+import headerLogo from "../../src/assets/images/headerLogo.png"
+
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='headerLogo'>
+        <NavLink to="/">
+          <img src={headerLogo} alt='header logo"' />
+        </NavLink>
+      </div>
+      <ul className='nav'>
+        <li className='nav-item item1'><NavLink className='nav-link' to='/' style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>Accueil</NavLink></li>
+        <li className='nav-item item2'><NavLink className='nav-link' to='/a-propos' style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>A Propos</NavLink></li>
+      </ul>
+    </div>
+
+  )
+}
