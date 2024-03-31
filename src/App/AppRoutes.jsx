a26b44562c6907555f99b4a27e944e8740ce9063
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-01 01:31:43 +0200

    change of directory

---
+++ src/App/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage.jsx';
+import AboutPage from '../pages/AboutPage/AboutPage.jsx';
+import Page404 from '../pages/Page404/Page404.jsx';
+import OfferDetailsPage from '../pages/OfferDetailsPage/OfferDetailsPage.jsx';
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/a-propos" element={<AboutPage />} />
+      <Route path="/locations/:rentalOfferId" element={<OfferDetailsPage />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  );
+}
