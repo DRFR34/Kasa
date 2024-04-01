9709c598a9a3503eab0a33f284c80529f63d9ecb
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-01 05:00:37 +0200

    Update Tags.jsx -   with BEM

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -5,7 +5,7 @@ export default function Tags({ rentalOfferTags }) {
   return (
     <div className='tagsContainer'>
       {rentalOfferTags.map((tag, index) => (
-        <span key={index} className='tagElet'>{tag}</span>))}
+        <span key={index} className='tagsContainer__tag'>{tag}</span>))}
     </div>
   )
 }
