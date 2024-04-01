2b9ac31c195f84aafa7d54e14259b97721209deb
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-01 20:24:23 +0200

    Improving router for github page, and fusioning 2 files in one

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
-import AppRoutes from './AppRoutes'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Header from '../components/Header/Header.jsx';
+import Footer from '../components/Footer/Footer.jsx';
+
+import HomePage from '../pages/HomePage/HomePage.jsx';
+import AboutPage from '../pages/AboutPage/AboutPage.jsx';
+import Page404 from '../pages/Page404/Page404.jsx';
+import OfferDetailsPage from '../pages/OfferDetailsPage/OfferDetailsPage.jsx';
 
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="headerAndMain">
-        <Header />
-        <AppRoutes />
-      </div>
-      <Footer />
-    </BrowserRouter>
+    <React.StrictMode>
+      <BrowserRouter basename="/Kasa">
+        <div className="headerAndMain">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/a-propos" element={<AboutPage />} />
+            <Route path="/locations/:rentalOfferId" element={<OfferDetailsPage />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </React.StrictMode>
   )
 }
--- src/App/oldFiles/AppRoutes.jsx
+++ src/App/oldFiles/AppRoutes.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage/HomePage.jsx';
-import AboutPage from '../pages/AboutPage/AboutPage.jsx';
-import Page404 from '../pages/Page404/Page404.jsx';
-import OfferDetailsPage from '../pages/OfferDetailsPage/OfferDetailsPage.jsx';
+
+import HomePage from '../../pages/HomePage/HomePage.jsx';
+import AboutPage from '../../pages/AboutPage/AboutPage.jsx';
+import Page404 from '../../pages/Page404/Page404.jsx';
+import OfferDetailsPage from '../../pages/OfferDetailsPage/OfferDetailsPage.jsx';
 
 export default function AppRoutes() {
   return (
