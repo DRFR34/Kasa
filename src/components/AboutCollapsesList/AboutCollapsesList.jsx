6fb355f954255cb6ae5de559da15e77cc163aa5f
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-01 05:04:28 +0200

    Update AboutCollapsesList.jsx

--- src/components/AboutCollapsesList/AboutCollapsesList.jsx
+++ src/components/AboutCollapsesList/AboutCollapsesList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {aboutCollapsesTexts} from "../../assets/data/aboutCollapsesTexts";
 
-import "./AboutCollapsesList.css"
+import "./AboutCollapsesList.scss"
 
 import Collapse from "../Collapse/Collapse";
 
