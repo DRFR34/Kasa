02634aec849f421e0793aa3e893f18b8105638b6
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-01 05:31:20 +0200

    Update  Header.jsx -   semantics HTML tags + BEM

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,20 +3,30 @@ import { NavLink } from 'react-router-dom';
 import './Header.scss';
 import headerLogo from "../../assets/images/headerLogo.png"
 
-
 export default function Header() {
   return (
-    <div className='header'>
-      <div className='headerLogo'>
+    <header className='header'>
+      <div className='header__logo'>
         <NavLink to="/">
-          <img src={headerLogo} alt='header logo"' />
+          <img src={headerLogo} alt="logo d'entête" />
         </NavLink>
       </div>
-      <ul className='nav'>
-        <li className='nav-item item1'><NavLink className='nav-link' to='/' style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>Accueil</NavLink></li>
-        <li className='nav-item item2'><NavLink className='nav-link' to='/a-propos' style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>A Propos</NavLink></li>
-      </ul>
-    </div>
-
+      <nav className='header__nav'>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? 'header__nav__link isActive' : 'header__nav__link'
+          }
+          to="/">
+          Accueil
+        </NavLink>
+        <NavLink
+          className={(({ isActive }) =>
+            isActive ? 'header__nav__link isActive' : 'header__nav__link'
+          )}
+          to="/a-propos">
+          A Propos
+        </NavLink>
+      </nav>
+    </header>
   )
 }
