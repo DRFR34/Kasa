35c9f7cac1c12232708a234246f0fea7a06f14a7
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-01 05:02:42 +0200

    Update Slider.jsx
    Deleting unnecessary comments

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -11,38 +11,34 @@ export default function Slider({ rentalOfferPictures }) {
 
   const previousSlide = () => {
     setCurrent(current === 0 ? rentalOfferPictures.length - 1 : current - 1);
-    
+
   };
 
   return (
     <div className="sliderContainer">
       {rentalOfferPictures.map((picture, index) => (
-        <div 
-          // className={index === current ? 'slide active' : 'slide'} 
+        <div
           className={
             index === current ? 'slide active' :
-            index === (current-1) ? 'slide previous':
-            // index === (current-1 && ) ? 'slide previous':
-            index === (current+1) ? 'slide next':
-            'slide'} 
+              index === (current - 1) ? 'slide previous' :
+                index === (current + 1) ? 'slide next' :
+                  'slide'}
           key={index}
         >
-          {/* {index === current && (
-            <img src={picture} alt={`référence ${index}`} />
-          )} */}
+
           {index === current ? <img src={picture} alt={`référence ${index}`} /> :
-            index === (current-1) ? <img src={picture} alt={`référence ${index-1}`} />:
-            index === (current+1) ? <img src={picture} alt={`référence ${index+1}`} />:
-            null
-            }  
+            index === (current - 1) ? <img src={picture} alt={`référence ${index - 1}`} /> :
+              index === (current + 1) ? <img src={picture} alt={`référence ${index + 1}`} /> :
+                null
+          }
 
         </div>
       ))}
       <div className="arrowBox previous" onClick={previousSlide}><i className="fa-solid fa-chevron-left"></i></div>
       <div className="arrowBox next" onClick={nextSlide} ><i className="fa-solid fa-chevron-right"></i></div>
       <div className="pictureRankBox" >
-        <span>{current + 1}</span>  
-        <span> / {rentalOfferPictures.length}</span>  
+        <span>{current + 1}</span>
+        <span> / {rentalOfferPictures.length}</span>
       </div>
     </div>
   );
