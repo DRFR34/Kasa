01cb34da9081b18d4f5398c46decce729d609f27
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-01 04:58:00 +0200

    Update OfferDetailsPage.jsx -   with semantics HTML tags + BEM

--- src/pages/OfferDetailsPage/OfferDetailsPage.jsx
+++ src/pages/OfferDetailsPage/OfferDetailsPage.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-
 import Page404 from '../Page404/Page404';
-
 import Slider from '../../components/Slider/Slider';
 import Tags from '../../components/Tags/Tags';
 import Collapse from '../../components/Collapse/Collapse'
 import Rating from '../../components/Rating/Rating';
-
 import RentalsData from '../../assets/data/rentalsData.json'
 
 import "./OfferDetailsPage.scss"
@@ -19,28 +16,28 @@ export default function RentalOfferDetails() {
     return (<div><Page404 /></div>)
   }
   return (
-    <div className='rentalOfferDetailsContainer'>
+    <main className='offerDetailsContainer'>
 
 
 
       <Slider rentalOfferPictures={rentalOffer.pictures} />
 
 
-      <div className="rentalOfferSummary">
-        <div className='title&Location'>
-          <h1 className='rentalOfferTitle'>{rentalOffer.title}</h1>
-          <span>{rentalOffer.location}</span>
+      <div className="offerDetailsContainer__Summary">
+        <div className='offerDetailsContainer__Summary__titleAndLocation'>
+          <h1 className='offerDetailsContainer__Summary__titleAndLocation__title'>{rentalOffer.title}</h1>
+          <span className='offerDetailsContainer__Summary__titleAndLocation__location'>{rentalOffer.location}</span>
           <Tags rentalOfferTags={rentalOffer.tags} />
         </div>
 
 
-        <div className="hostInfos">
-          <div className="hostIdentity">
-            <div className="hostName">
+        <div className="offerDetailsContainer__Summary__hostInfos">
+          <div className="offerDetailsContainer__Summary__hostInfos__identity">
+            <div className="offerDetailsContainer__Summary__hostInfos__identity__name">
               <span>{rentalOffer.host.name.split(' ')[0]}</span>
               <span>{rentalOffer.host.name.split(' ')[1]}</span>
             </div>
-            <div className="hostPortrait">
+            <div className="offerDetailsContainer__Summary__hostInfos__identity__portrait">
               <img src={rentalOffer.host.picture} alt="portrait de l'hôte"></img>
             </div>
           </div>
@@ -48,12 +45,11 @@ export default function RentalOfferDetails() {
         </div>
       </div>
 
-      <div className='rentalOfferCollapses'>
+      <div className='offerDetailsContainer__collapses'>
         <Collapse title='Description' content={rentalOffer.description} />
         <Collapse title='Équipements' content={rentalOffer.equipments} />
-
       </div>
 
-    </div>
+    </main>
   )
 }
