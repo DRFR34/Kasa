24aa9adf924e9c904877125b633c42d296053f8a
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-01 02:41:55 +0200

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
-import AppRoutes from '../components/AppRoutes/AppRoutes'
+import AppRoutes from './AppRoutes'
 
 
 
